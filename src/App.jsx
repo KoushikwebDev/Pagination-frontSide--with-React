c7323ce42929ce34ee928e25b60955342f9801e0
KoushikwebDev <kou.saha.ami@gmail.com>
2023-02-12 15:14:23 +0530

    pagination done

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(5);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPost();
+  }, []);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (number) => {
+    setCurrentPage(number);
+  };
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">Pagination</h1>
+      <Posts posts={currentPosts} loading={loading} />
+
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+    </div>
+  );
+}
+
+export default App;
